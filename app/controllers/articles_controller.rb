d6edce3603e492affbaea2f22e46ff537c10b112
mimi peshy <mimipeshy@gmail.com>
2020-08-24 22:36:11 +0300

    form based view rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,16 @@ class ArticlesController < ApplicationController
     end
     def show
         @article = Article.find(params[:id])
+    end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params[:article])
+        @article.save
+        redirect_to article_path(@article)
     end
     
       
