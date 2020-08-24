17b8176dde0331b49e1469133033354b3c04aebd
kossySteve <kossyeze@gmail.com>
2020-08-24 18:49:01 +0100

    added comment feature to blog app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
     end
     def show
         @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
+
     end
 
     def new
